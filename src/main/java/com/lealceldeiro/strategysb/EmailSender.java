26ab87052363c5efe609ff34bf3db6b7c8bd42c5
Asiel Leal Celdeiro <15990580+lealceldeiro@users.noreply.github.com>
2024-07-02 23:28:11 +0200

    Add logic to send emails

--- src/main/java/com/lealceldeiro/strategysb/EmailSender.java
+++ src/main/java/com/lealceldeiro/strategysb/EmailSender.java
@@ -1,10 +1,52 @@
 package com.lealceldeiro.strategysb;
 
+import com.mailersend.sdk.MailerSend;
+import com.mailersend.sdk.MailerSendResponse;
+import com.mailersend.sdk.emails.Email;
+import com.mailersend.sdk.exceptions.MailerSendException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import java.util.logging.Logger;
 
 @Component
 public class EmailSender {
+  private static final Logger LOGGER = Logger.getLogger(EmailSender.class.getName());
+
+  @Value("${integration.mailersend.token}")
+  private String token;
+
   public boolean send(String to, String subject, String body) {
-    return false;
+    Email email = createEmail(to, subject, body);
+    MailerSend sender = createSender();
+
+    return sendEmailUsingSender(sender, email);
+  }
+
+  private static Email createEmail(String to, String subject, String body) {
+    Email email = new Email();
+    email.setFrom("Strategy Pattern In Spring Boot", "comlealceldeiro@strategy.com");
+
+    email.addRecipient(to, to);
+    email.setSubject(subject);
+    email.setPlain(body);
+    return email;
+  }
+
+  private MailerSend createSender() {
+    MailerSend ms = new MailerSend();
+    ms.setToken(token);
+    return ms;
+  }
+
+  private static boolean sendEmailUsingSender(MailerSend sender, Email email) {
+    try {
+      MailerSendResponse response = sender.emails().send(email);
+      LOGGER.info("Attempting to send email " + response.messageId);
+    } catch (MailerSendException e) {
+      LOGGER.warning("Email not sent");
+      return false;
+    }
+
+    return true;
   }
 }
